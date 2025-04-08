44bfaedcbaa6244abea9830666b74bc18108154c
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-04-08 15:21:46 +0530

    Improvement 04

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(); // used for middlewares & configuration
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-// require('dotenv').config({path: "./env"})
-
-// import mongoose from "mongoose";
-// import express from "express";
-// import { DB_NAME } from "./constants";
+// require('dotenv').config({path: "./env"});
 
 import dotenv from "dotenv";
 import ConnectDB from "./db/database.js";
 
+import app from "./app.js";
+
 dotenv.config({path: "./env"})
 
-ConnectDB();
+const port = process.env.PORT || 8000;
+
+ConnectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`⚙️ Server is running at port ${port}`);
+        })
+    })
+    .catch((err) => console.log("MongoDatabase Error !!!", err));
+
+
+
+
+
 
 // async function main() {
 //     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
