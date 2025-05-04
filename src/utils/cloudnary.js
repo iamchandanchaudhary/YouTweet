23538d7a2b10e3b3508e28f18f6bbe7f791beee9
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-05-04 08:21:30 +0530

    Improvement 10

---
+++ src/utils/cloudnary.js
@@ -0,0 +1,8 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: 'chandanchaudahry', 
+    api_key: '355259918652266', 
+    api_secret: 'XJaZarTKSd1S0sK9nPt2lRDXDf0'
+});
